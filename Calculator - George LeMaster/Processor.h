c36beb685c9ef986001fd861efa9e3f1edb9f181
George LeMaster <GDLeMaster@student.fullsail.edu>
2022-05-20 17:14:13 -0400

    Merged Calculator Processor into Master

---
+++ Calculator - George LeMaster/Processor.h
@@ -0,0 +1,39 @@
+#pragma once
+class Processor
+{
+
+public:
+	Processor(const Processor& _copy) = delete;
+	Processor& operator=(const Processor& _assign) = delete;
+
+	static Processor* GetInstance()
+	{
+		if (_processor == nullptr)
+		{
+			_processor = new Processor();
+		}
+
+		return _processor;
+	}
+
+	float numOne;
+	float numTwo;
+	int operatorIndex;
+
+	bool hasOperator = false;
+
+	float Add() { return numOne + numTwo; }
+	float Subtract() { return numOne - numTwo; }
+	float Multiply() { return numOne * numTwo; }
+	float Divide() { return numOne / numTwo; }
+	int MOD() { return (int)numOne % (int)numTwo; }
+
+	void SetOperator(int input);
+
+	~Processor() { if (_processor != nullptr) { delete _processor; } }
+
+private:
+	Processor();
+	static Processor* _processor;
+};
+
